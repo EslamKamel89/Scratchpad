aaaadbd181bee124f3abff3fa84d9d56512651f7
EslamKamel89 <elsamkamelforex@gmail.com>
2025-10-16 18:20:56 +0300

    feat(format): add Prettier with Tailwind plugin
    - Install prettier and prettier-plugin-tailwindcss
    - Configure .prettierrc to enable Tailwind sorting
    - Apply basic UI and global background style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <>
-      <div className="text-xl text-red-500">hello world</div>
-    </>
+    <div className="mx-auto mt-10 max-w-lg rounded-lg bg-gray-100 p-6 shadow-lg">
+      <h2 className="mb-4 text-center text-2xl font-bold">Scratch Pad 🖋️</h2>
+    </div>
   );
 }
 
